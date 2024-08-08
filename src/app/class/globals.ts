90ae75783b1126ee9b7316b2c7c5e226b31e7786
omarkhaled192 <okoros192000@gmail.com>
2024-08-08 11:55:00 +0300

    adding-description-for-building-global-class

--- src/app/class/globals.ts
+++ src/app/class/globals.ts
@@ -1,5 +1,6 @@
 import { BehaviorSubject, Observable } from "rxjs";
 
+// this class build specialy for Translations, To Lens On Any Changes Cause In Translation fields.
 export abstract class Globals {
     public static mainLang: BehaviorSubject<string> = new BehaviorSubject<string>("ar");
 
