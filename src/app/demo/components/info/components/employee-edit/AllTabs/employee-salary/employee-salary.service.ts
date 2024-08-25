914e9f3860fa33125ad640716f2c216ecbb9ce9f
omarkhaled <okoros192000@gmail.com>
2024-08-26 02:12:12 +0300

    add-employee-salary

--- src/app/demo/components/info/components/employee-edit/AllTabs/employee-salary/employee-salary.service.ts
+++ src/app/demo/components/info/components/employee-edit/AllTabs/employee-salary/employee-salary.service.ts
@@ -6,17 +6,10 @@ import { LockupsService } from 'src/app/demo/service/lockups.service';
 @Injectable({
     providedIn: 'root',
 })
-export class EmployeeManagerService extends LockupsService {
+export class EmployeeSalaryService extends LockupsService {
     constructor(http: HttpClient) {
         super(http);
     }
 
-    getDropDownManager(field: string) {
-        return this.http.get(`${this.baseurl}/${field}/GetMangerDropDown`);
-    }
-
-    getDropDown(field: string) {
-        return this.http.get(`${this.baseurl}/${field}/getDropDown`);
-    }
 
 }
