92c5a65b36859575246028cd0257f77d14568ab0
omar khaled <94392724+OmarKhaled192@users.noreply.github.com>
2024-10-21 12:45:22 +0300

    Update auth.interceptor.ts

--- src/app/demo/components/auth/auth.interceptor.ts
+++ src/app/demo/components/auth/auth.interceptor.ts
@@ -2,28 +2,28 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
-import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (request, next) => {
-   
+
     const token = localStorage.getItem('userToken') ;
     const router = inject(Router);
+
     if (token) {
     const decodedToken = jwtDecode(token)  ;
+
     const curDate = new Date(); // Get current date and time
     const expDate: Date = new Date(decodedToken.exp * 1000);  // Convert token expiration to Date
-   
+
     console.log('Expiration Date:', expDate);
     console.log('Current Date:', curDate);
-    
+
     // Check if the token has expired
     if (expDate < curDate) {
         console.log('Token has expired.');
         localStorage.removeItem('userToken');
         router.navigate(['/auth/login'])
-    } 
+    }
 }
-
 
     const req = request.clone({
         setHeaders: {
@@ -31,6 +31,5 @@ export const authInterceptor: HttpInterceptorFn = (request, next) => {
         },
     });
 
-    return next(req).pipe(
-    );
+    return next(req).pipe();
 };
