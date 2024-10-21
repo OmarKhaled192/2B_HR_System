c6aab93ee429c696511cdaac524787fdd2378c5c
omar khaled <94392724+OmarKhaled192@users.noreply.github.com>
2024-10-21 12:49:45 +0300

    Update auth.guard.ts
    Add Authorize for Admin & User

--- src/app/demo/components/auth/auth.guard.ts
+++ src/app/demo/components/auth/auth.guard.ts
@@ -1,24 +1,55 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { MessageService } from 'primeng/api';
+import { isArray } from 'lodash';
 
 export const authGuard: CanActivateFn = (route, state) => {
     let router = inject(Router);
-    const isLoggedIn = !!localStorage.getItem('userToken'); // Boolean check for the token
+    let messageService = inject(MessageService);
+    const token = localStorage.getItem('userToken') ; // Boolean check for the token
 
-    console.log(isLoggedIn);
-    console.log(state);
+    // console.log(token);
+    // console.log(state);
+
+    let decodedToken = {};
+
+    if(token) {
+        decodedToken = jwtDecode(token) ;
+        console.log("decoded Token Gurad => ", decodedToken?.['role'])
+    }
+
+    const Role = decodedToken?.['role'];
 
     // Prevent navigating back to the login page if already logged in
-    if (isLoggedIn && state.url === '/auth/login') {
+    if (token && state.url === '/auth/login') {
         router.navigate(['/']);
         return false;
-    } 
-  
+    }
+
+    // // If the user is an Employee, redirect to login
+    if (token &&
+        (
+            Role === 'Employee'
+            ||  (isArray(Role) && Role[0] == 'Employee' && Role.length == 1)
+        )
+         ) {
+        localStorage.removeItem("userToken");
+        messageService.add({
+            severity: 'error',
+            summary: 'Error, you are not authorized',
+            detail: 'You must be an admin',
+            life: 3000 // Show for 3 seconds
+          });
+
+        return false;
+    }
+
     // If not logged in, redirect to login
-    else if (!isLoggedIn && state.url !== '/auth/login') {
+    if (!token && state.url !== '/auth/login') {
         router.navigate(['/auth/login']);
         return false;
-    } else {
-        return true;
     }
+
+    return true; // Allow navigation for other cases
 };
