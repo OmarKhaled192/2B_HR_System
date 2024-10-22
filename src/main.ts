e7ae84b12ece953ab81943a1d390d31249f6d244
EzzAly74 <127905343+EzzAly74@users.noreply.github.com>
2024-10-22 15:14:45 +0300

    editing-popup

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ if (environment.production) {
   enableProdMode();
 }
 
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
 
 
 export let itemsPerPageGlobal = 10 ;
